b134f667edcecb3c1f6ed9b9d5771b0756710de7
lololo514 <1198657086@qq.com>
2024-10-17 21:20:02 +0800

    weather report

--- src/components/NoResultsDiv.jsx
+++ src/components/NoResultsDiv.jsx
@@ -2,7 +2,7 @@ const NoResultsDiv = () => {
     return <div className="no-results ">
         <img src="icons/no-results.png" alt="No results found" 
         className="iocn"/>
-        <h3 className="titel" >无事发生</h3>
+        <h3 className="titel" >没有找到对应城市</h3>
         <p className="message">请输入有效的城市名称</p>
     </div>;
 };
